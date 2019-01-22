865369b2a3f1eeef3ea0e571a3e58cffa20c6ec4
balazsDombovari <dbalazs1219@gmail.com>
2019-01-22 16:36:08 +0100

    "post answer" user story. It's done to step: 6. Data got from post_answer.html post answer.html is done server.py: added: new_answer() works for POST,GET

--- server.py
+++ server.py
@@ -11,5 +11,15 @@ def route_list():
     return render_template('list.html', dict=data_logic.get_all_questions())
 
 
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
+def new_answer(question_id):
+    if request.method == 'POST':
+        new_answer = request.form.get('new_answer')
+        print(new_answer)
+        return redirect('/question/' + question_id)
+
+    return render_template("post_answer.html", q_id=question_id)
+
+
 if __name__ == "__main__":
     app.run()

b9c417cb0ab4abfa54d0f7881f8dffb219384714
balazsDombovari <dbalazs1219@gmail.com>
2019-01-22 13:47:17 +0100

    server.py: import fix route_list(): argument updated.

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect
-import connection
+import data_logic
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/list')
 def route_list():
-    return render_template('list.html')
+    return render_template('list.html', dict=data_logic.get_all_questions())
+
 
 if __name__ == "__main__":
     app.run()
